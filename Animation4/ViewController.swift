12e5460dc913a6093c8ca22bd117c496bffc8198
Erwin Warkentin <niwrE91@dergerat-2.local>
2019-06-27 11:05:03 +0200

    Initial Commit

---
+++ Animation4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation4
+//
+//  Created by Erwin Warkentin on 27.06.19.
+//  Copyright © 2019 Erwin Warkentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
